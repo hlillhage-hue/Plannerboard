b89c9d5c574ffe97773b90d690fb0376635bd65d
hlillhage-hue <h.lillhage@gmail.com>
2026-05-22 13:31:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,16 @@
-var CACHE = "postit-v2";
+var CACHE = "postit-v3";
 var FILES = ["./index.html", "./manifest.json"];
 
 self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(FILES);
-    })
-  );
+  e.waitUntil(caches.open(CACHE).then(function(cache) { return cache.addAll(FILES); }));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
-    })
-  );
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request); }));
 });
